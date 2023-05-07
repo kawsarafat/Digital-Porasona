6536d0c007a318e5ab4b9f98a95ad58ffdc10115
kawsarafat <77534968+kawsarafat@users.noreply.github.com>
2023-05-07 14:43:34 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+<script>
+jQuery(document).ready(function() {
+var sec = 300
+var timer = setInterval(function() {
+   $("#mdtimer span").text(sec--);
+   if (sec == 0) {
+$("#makingdifferenttimer").delay(1000).fadeIn(1000);
+$("#mdtimer").hide(1000) .fadeOut(fast);}
+},1000);
+});
+</script>
